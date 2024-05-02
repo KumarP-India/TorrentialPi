c67db5c74d59294cb5685eddd8ad34bba759bece
KumarP-India <kumar.prabhas.india@gmail.com>
2024-05-03 03:00:22 +0530

    Completed the Developer.py. Pending real life testing along with all scripts and files (after I sleep)

--- Programmes/DeveloperApp.py
+++ Programmes/DeveloperApp.py
@@ -1,12 +1,299 @@
 import tkinter as tk
 from tkinter import ttk
-import json
 import re
 import subprocess
+import argparse
+from prompt_toolkit import prompt
+from prompt_toolkit.shortcuts import radiolist_dialog
+from prompt_toolkit.shortcuts import checkboxlist_dialog
+import json
 
 
 from _Email import send_email
 from tkinter import messagebox
+
+
+class Sex:
+
+    def __init__(self) -> None:
+        pass
+
+    def Pornstar(self):
+
+        penis = "Welcome.\n\nPlease select what you want to do.\n\n"
+
+        holes = [
+            ('boobs', 'To clear log files.'),
+            ('blow', 'To export log files.'),
+            ('ass', 'To turn VNC on or off for Remote Desktop Acess.'),
+            ('oral', 'To get reccmendation on how to free up disk space.'),
+            ('pussy', 'To change Settings (Settings.json).'),
+            ('Quit.', 'q')
+        ]
+
+        porn = radiolist_dialog(
+            title=penis,
+            values=holes
+        ).run()
+
+
+        match porn:
+
+            case 'boobs': self.BoobGrab()
+
+            case 'blow': self.Blowjob()    
+
+            case 'ass': self.Anal()
+
+            case 'oral': self.OralSex()
+
+            case 'pussy': self.Pussy()
+
+            case 'q': return
+
+            case _: raise RuntimeError('invalid command line main menu')
+
+        
+    def BoobGrab(self):
+
+        # log files
+        scripts_log = '../Status_and_Logs/Scripts.log'
+        transfered_log = '../Status_and_Logs/transfered.log'
+
+        options = [
+            ('boob', 'Universal log file (Scripts.log)'),
+            ('nipple', 'Transfer History (transfered.log)'),
+        ]
+
+        tits = checkboxlist_dialog(
+            title='Select which file(s) you want to clear (Press Space to toggle selection, Enter/Return to confirm)\n\n',
+            values=options
+        ).run()
+
+        if not tits:
+
+            print("None of the log files cleared.\n\n")
+
+            self.Pornstar()
+
+        else:
+                    
+            if radiolist_dialog(
+                title=f'Are you sure you want to clear: {tits}?',
+                values=[
+                    (0, 'Yes'),
+                    (1, 'No')
+                ]
+                ).run():
+
+                if 'boob' in tits:
+
+                    with open(scripts_log, 'w') as file:
+
+                        file.write('')
+
+                        print('Scripts.log cleared')
+
+                if 'nipple' in tits:
+
+                    with open(transfered_log, 'w') as file:
+
+                        file.write('')
+
+                        print('transfered.log cleared')
+
+            else: 
+            
+                print("None of the log files cleared.\n\n")
+
+                self.Pornstar()
+
+
+    def Blowjob(self):
+
+        # log files
+        scripts_log = '../Status_and_Logs/Scripts.log'
+        transfered_log = '../Status_and_Logs/transfered.log'
+        cleaner_recommendation = '../Status_and_Logs/CleanerRecommendation.list'
+
+
+        options = [
+            ('lick', 'Universal log file (Scripts.log)'),
+            ( 'deepthroat', 'Transfer History (transfered.log)'),
+            ( 'cum', 'Last Cleaning Recommendation (CleanerRecommendation.list)')
+        ]
+
+        mouth = checkboxlist_dialog(
+            title='Select which file(s) you want to send (Press Space to toggle selection, Enter/Return to confirm)\n\n',
+            values=options
+        ).run()
+
+        if not mouth:
+
+            print("None of the log files sent.\n\n")
+
+            self.Pornstar()
+
+        else:
+
+            blowjob = []
+
+            if 'lick' in mouth:
+
+                blowjob.append(scripts_log)
+
+            if 'deepthroat' in mouth:
+
+                blowjob.append(transfered_log)
+
+            if 'cum' in mouth:
+
+                blowjob.append(cleaner_recommendation)
+            
+            if radiolist_dialog(
+                title=f'Are you sure you want to send: {blowjob}?',
+                values=[
+                    (0, 'Yes'),
+                    (1, 'No')
+                ]
+                ).run():
+
+                    send_email(blowjob)
+
+                    print(f"{blowjob} file(s) sent.")
+
+                
+
+            else: 
+            
+                print("None of the log files sent.\n\n")
+
+                self.Pornstar()
+
+
+    def Anal(self):
+
+        if radiolist_dialog(
+            title=f'Are you sure you want to turn VNC {self.is_vnc_on()}?',
+            values=[
+                (0, 'Yes'),
+                (1, 'No')
+            ]
+            ).run():
+
+            if self.is_vnc_on() == 'on':
+                subprocess.run(['systemctl', 'stop', 'vncserver'])
+            else:
+                subprocess.run(['systemctl', 'start', 'vncserver'])
+
+        print(f"VNC is {self.is_vnc_on()}")
+
+        self.Pornstar()
+
+
+    def OralSex(self):
+
+        cum = True
+
+        clit = '../Status_and_Logs/CleanerRecommendation.list'
+
+        print("Here are previous recommendations:\n\n")
+
+        while cum:
+
+            with open(clit) as f:
+
+                for i in f.readlines: print(i, end='\n\n')
+
+                if radiolist_dialog(
+                    title=f'\n\nDo you want to regenerate recommendations?',
+                    values=[
+                        (1, 'Yes'),
+                        (0, 'No')
+                    ]
+                    ).run():
+                
+                    subprocess.run(['../Scripts/CleanerScript.sh'])
+
+                else: self.Pornstar()
+
+    
+    def Pussy(self):
+
+        print("Please note there are settings that even after updating won't make a difference beacuse setup.py once run send all files to where that are supposed to be and you need to change in every one of them.\n\nWhen you want to exit and save changed settings - press Cmd + c then type [Y or y]", end='\n\n')
+
+        pussy = {}
+
+        try:
+
+            for i, j in json.load(open('../Settings.json()')).items:
+
+                if input(f'Curently "{i}" is set to "{j}" . Do you want to change it? [Y/n]: ').lower() == 'n': continue
+
+                orgasm = input(f'\nNew value for {i}: ')
+
+                if not orgasm: 
+                    
+                    print("No value given. Skiping {i} with its current value - {j}")
+
+                    continue
+
+                try:
+
+                    orgasm = int(orgasm)
+
+                except ValueError:
+
+                    pass
+
+                pussy[i] = orgasm
+
+        
+        except KeyboardInterrupt:
+
+            stuffed = input("Do you want to save changes? [Y/n]: ").lower()
+
+            if stuffed == 'n':
+
+                print("No changes made.\n\n")
+
+                self.Pornstar()
+
+                return
+
+
+        
+        finally:
+
+            if not pussy:
+
+                print("No changes made.\n\n")
+
+                self.Pornstar()
+
+            else:
+
+                with open('../Settings.json', 'w') as creamipi:
+
+                    moan = json.load(creamipi)
+
+                    moan.update(pussy)
+
+                    json.dump(moan, creamipi)
+
+                print("Settings updated successfully.\n\n")
+
+                self.Pornstar()
+
+                
+
+
+    def is_vnc_on(self):
+
+        result = subprocess.run(['systemctl', 'is-active', 'vncserver'], capture_output=True, text=True)
+        return "on" if result.stdout.strip() == 'active' else "off"
+
+
 
 
 
@@ -219,7 +506,7 @@ Creates a frame for managing VNC Remote Desktop Access.
         cheek.pack(pady=10)
 
         try:
-            with open('~/Developer/Softwares/My work/IoT/TorrentialPi/Status_and_Logs/CleanerRecommendation.list', 'r') as file:
+            with open('../Status_and_Logs/CleanerRecommendation.list', 'r') as file:
                 prostate = file.read()
         except FileNotFoundError:
             prostate = "Recommendation list not found."
@@ -277,8 +564,8 @@ Clears the selected log files.
         '''
 
         # log files
-        scripts_log = '~/Developer/Softwares/My work/IoT/TorrentialPi/Status_and_Logs/Scripts.log'
-        transfered_log = '~/Developer/Softwares/My work/IoT/TorrentialPi/Status_and_Logs/transfered.log'
+        scripts_log = '../Status_and_Logs/Scripts.log'
+        transfered_log = '../Status_and_Logs/transfered.log'
 
         if messagebox.askyesno("Confirm", f"Are you sure you want to clear these files?"):
 
@@ -309,9 +596,9 @@ Sends the selected log files.
         '''
 
         # log files
-        scripts_log = '~/Developer/Softwares/My work/IoT/TorrentialPi/Status_and_Logs/Scripts.log'
-        transfered_log = '~/Developer/Softwares/My work/IoT/TorrentialPi/Status_and_Logs/transfered.log'
-        cleaner_recommendation = '~/Developer/Softwares/My work/IoT/TorrentialPi/Status_and_Logs/CleanerRecommendation.list'
+        scripts_log = '../Status_and_Logs/Scripts.log'
+        transfered_log = '../Status_and_Logs/transfered.log'
+        cleaner_recommendation = '../Status_and_Logs/CleanerRecommendation.list'
 
         # Send the selected log files
         
@@ -335,7 +622,7 @@ Sends the selected log files.
             
             if messagebox.askyesno("Confirm", f"Are you sure you want to send these files?"):
 
-                #send_email(nipples)
+                send_email(nipples)
 
                 print('Email sent successfully!')
 
@@ -365,11 +652,32 @@ Sends the selected log files.
         Refreshes the cleaning recommendation.
         '''
 
-        subprocess.run(['~/Developer/Softwares/My work/IoT/TorrentialPi/Scripts/CleanerScript.sh'])
+        subprocess.run(['../Scripts/CleanerScript.sh'])
 
         self.ButtKiss_()
 
+if __name__ == '__main__':
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description='Developer App')
+
+    # Add a flag argument (--shell) that defaults to False
+    parser.add_argument('--shell', action='store_true', help='Flag to run command line script')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Access the value of the flag argument
+    if args.shell:
+
+        print("Skippling GUI, starting command line interface.")
+
+        fuck = Sex()
+
+        fuck.Pornstar()
 
 
-app = Porn("Dev app", "600x300")
-app.Kiss()
+    else:
+        print("Starting GUI")
+
+        app = Porn("Dev app", "600x300")
+        app.Kiss()
